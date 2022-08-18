7603ad1afe9ecb435cbc5887561bb4fd9ec4b09f
Lucas Nunes <lucasnsilveira202@gmail.com>
2022-08-18 11:05:25 -0300

    Implementação das funções density() e oriented()

--- src/App.java
+++ src/App.java
@@ -11,7 +11,7 @@ public class App {
         g.addEdge(0, 2, 3);
 
         System.out.println(g);
-        
+
         System.out.println("Num de Vizinhos: " + g.alfaDegree(0));
         System.out.println("Num de Vizinhos: " + g.alfaDegree(1));
         System.out.println("Num de Vizinhos: " + g.alfaDegree(2));
@@ -25,30 +25,46 @@ public class App {
         System.out.println("\n" + complementGraph.toString());
 
         Graph g1 = new Graph(4);
-      g1.addEdge(0,1,1);g1.addEdge(0,3,1);
-      g1.addEdge(1,0,1);g1.addEdge(3,0,1);
-      System.out.println(g1);
+        g1.addEdge(0, 1, 1);
+        g1.addEdge(0, 3, 1);
+        g1.addEdge(1, 0, 1);
+        g1.addEdge(3, 0, 1);
+        System.out.println("Matriz de G1\n" + g1);
 
-      Graph g2 = new Graph(4);
-      g2.addEdge(0,2,1);g2.addEdge(2,0,1);
-      g2.addEdge(1,2,1);g2.addEdge(2,1,1);
-      g2.addEdge(2,3,1);g2.addEdge(3,2,1);
-      g2.addEdge(1,3,1);g2.addEdge(3,1,1);
-      System.out.println(g2);
+        Graph g2 = new Graph(4);
+        g2.addEdge(0, 2, 1);
+        g2.addEdge(2, 0, 1);
+        g2.addEdge(1, 2, 1);
+        g2.addEdge(2, 1, 1);
+        g2.addEdge(2, 3, 1);
+        g2.addEdge(3, 2, 1);
+        g2.addEdge(1, 3, 1);
+        g2.addEdge(3, 1, 1);
+        System.out.println("Matriz de G2\n" +g2);
 
-      Graph g3 = new Graph(4);
-      g3.addEdge(0,3,1);g3.addEdge(3,0,1);
-      System.out.println(g3);
+        Graph g3 = new Graph(4);
+        g3.addEdge(0, 3, 1);
+        g3.addEdge(3, 0, 1);
+        System.out.println("Matriz de G3\n" +g3);
 
-      Graph g4 = new Graph(4);
-      g4.addEdge(0,1,1);g4.addEdge(1,0,1);
-      g4.addEdge(2,3,1);g4.addEdge(3,2,1);
-      System.out.println(g4);
+        Graph g4 = new Graph(4);
+        g4.addEdge(0, 1, 1);
+        g4.addEdge(1, 0, 1);
+        g4.addEdge(2, 3, 1);
+        g4.addEdge(3, 2, 1);
+        System.out.println("Matriz de G4\n" +g4);
 
-      System.out.println("G1 é SubGrafo de G2? = " + g1.subGraph(g2));
-      System.out.println("G1 é SubGrafo de G3? = " + g1.subGraph(g3));
-      System.out.println("G2 é SubGrafo de G3? = " + g2.subGraph(g3));
-      System.out.println("G1 é SubGrafo de G4? = " + g1.subGraph(g4));
-        
+        System.out.println("G1 é SubGrafo de G2? = " + g1.subGraph(g2));
+        System.out.println("G1 é SubGrafo de G3? = " + g1.subGraph(g3));
+        System.out.println("G2 é SubGrafo de G3? = " + g2.subGraph(g3));
+        System.out.println("G1 é SubGrafo de G4? = " + g1.subGraph(g4));
+
+        Graph G5 = new Graph(4);
+        G5.addEdge(0, 1, 1);
+        G5.addEdge(1, 0, 1);
+        G5.addEdge(3, 0, 1);
+        G5.addEdge(0, 3, 1);
+        System.out.printf("\nDensidade de G5: %.2f", G5.density());
+        System.out.println("\nO Grafo G5 é orientado?: " + G5.oriented());
     }
 }
--- src/Graph.java
+++ src/Graph.java
@@ -5,61 +5,61 @@ public class Graph {
     private int countNodes;
     private int countEdges;
     private int[][] adjMatriz;
-    
-    public Graph(int numNodes){
+
+    public Graph(int numNodes) {
         this.countNodes = numNodes;
         this.countEdges = 0;
         this.adjMatriz = new int[numNodes][numNodes];
     }
 
-    public void addEdge(int source, int sink, int weight){
-        if(source < 0 || source >= this.adjMatriz.length || sink < 0 || sink >= this.adjMatriz.length || weight <=0){
+    public void addEdge(int source, int sink, int weight) {
+        if (source < 0 || source >= this.adjMatriz.length || sink < 0 || sink >= this.adjMatriz.length || weight <= 0) {
             System.out.println("\n!!! Invalid edg: So: " + source + " /Si: " + sink + " /We: " + weight + "\n");
             return;
-        } 
+        }
         this.countEdges++;
         this.adjMatriz[source][sink] = weight;
     }
 
-    public int alfaDegree(int node){
-        if(node >= 4){
+    public int alfaDegree(int node) {
+        if (node >= 4) {
             System.out.println("\n !!! Invalid node number !!!");
             return -1;
         }
         int count = 0;
-        for(int i=0; i<this.adjMatriz.length; i++){
-            if(this.adjMatriz[node][i] != 0){
-                count ++;
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            if (this.adjMatriz[node][i] != 0) {
+                count++;
             }
         }
         return count;
     }
 
-    public int highestDegree(){
+    public int highestDegree() {
         int count = 0;
-        for(int i=0; i<this.adjMatriz.length; i++){
-            if(count < this.alfaDegree(i)){
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            if (count < this.alfaDegree(i)) {
                 count = this.alfaDegree(i);
             }
         }
         return count;
     }
 
-    public int lowestDegree(){
+    public int lowestDegree() {
         int count = 0;
-        for(int i=0; i<this.adjMatriz.length; i++){
-            if(count > this.alfaDegree(i)){
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            if (count > this.alfaDegree(i)) {
                 count = this.alfaDegree(i);
             }
         }
         return count;
     }
-    
-    public Graph complement(){
+
+    public Graph complement() {
         Graph g = new Graph(this.countNodes);
-        for(int i=0; i<this.adjMatriz.length; i++){
-            for(int j=0; j<this.adjMatriz[i].length; j++){
-                if((this.adjMatriz[i][j] == 0) && (i!=j)){
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            for (int j = 0; j < this.adjMatriz[i].length; j++) {
+                if ((this.adjMatriz[i][j] == 0) && (i != j)) {
                     g.addEdge(i, j, 1);
                 }
             }
@@ -67,13 +67,13 @@ public class Graph {
         return g;
     }
 
-    public boolean subGraph(Graph g2){
-        if(g2.countEdges > this.countEdges || g2.countNodes > this.countNodes){
+    public boolean subGraph(Graph g2) {
+        if (g2.countEdges > this.countEdges || g2.countNodes > this.countNodes) {
             return false;
         }
-        for(int i=0; i<this.adjMatriz.length; i++){
-            for(int j=0; j<this.adjMatriz.length; j++){
-                if((g2.adjMatriz[i][j] != 0) && this.adjMatriz[i][j] == 0){
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            for (int j = 0; j < this.adjMatriz.length; j++) {
+                if ((g2.adjMatriz[i][j] != 0) && this.adjMatriz[i][j] == 0) {
                     return false;
                 }
             }
@@ -81,11 +81,31 @@ public class Graph {
         return true;
     }
 
+    public float density() {
+        float d = 0;
+        d = (float) this.countEdges / (this.countNodes * (this.countNodes - 1));
+        return d;
+    }
+
+    public boolean oriented() {
+
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            for (int j = 0; j < this.adjMatriz.length; j++) {
+                if (adjMatriz[i][j] == adjMatriz[j][i] && adjMatriz[i][j] == 1) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+
+    }
+
     @Override
     public String toString() {
         String str = "";
-        for(int i=0; i<this.adjMatriz.length; i++){
-            for(int j=0; j<this.adjMatriz.length; j++){
+        for (int i = 0; i < this.adjMatriz.length; i++) {
+            for (int j = 0; j < this.adjMatriz.length; j++) {
                 str += this.adjMatriz[i][j] + "\t";
             }
             str += "\n";
